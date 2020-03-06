09d1650b83274cd90c9824dd61d19d510cf51bc6
ALVARO-JOSE <alvaro_jose0408@hotmail.com>
2020-03-06 11:30:46 -0600

    actualisacion

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import Carruse from './carruse';
 import Tex from './tex';
+import '../imagenes/galeria.css';
 
 class home extends Component {
     render() {
         return (
             <div>
-   <Carruse/>
+                <div className="row carousel">
+                <Carruse/>
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-3">
+                        Columna 1
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 2
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 3
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 4
+                    </div>
+
+                </div>
+                <div className="row">
+                    <div className="col-lg-3">
+                        Columna 1
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 2
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 3
+                    </div>
+                    <div className="col-lg-3">
+                        Columna 4
+                    </div>
+
+                </div>
+   
    
                 
             </div>
