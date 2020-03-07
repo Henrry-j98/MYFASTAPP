dd9f8fe1e7db02d9b3e283ba7a2e1b90ca3c0b3d
ALVARO-JOSE <alvaro_jose0408@hotmail.com>
2020-03-06 21:54:28 -0600

    actualisacion del carrusel

--- src/pages/carruse.js
+++ src/pages/carruse.js
@@ -18,6 +18,8 @@ class carruse extends Component {
     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
 
+
+
   </ol>
   <div class="carousel-inner"/>
     <div class="carousel-item active">
